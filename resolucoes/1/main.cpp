4c08fd7d6fb14d8dad3561e7282f3f0ec31230ff
Israel David <israelcaetano21@hotmail.com>
2022-12-12 20:45:30 -0300

    style: formats (Google convention) fix wording

--- resolucoes/1/main.cpp
+++ resolucoes/1/main.cpp
@@ -1,108 +1,102 @@
-#include <iostream>
-#include <cstdlib>
-#include <vector>
 #include <math.h>
+
+#include <cstdlib>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 // fills a matrix with random numbers from 1 to maxNum
-void fillMatrix(vector<vector<int>>& matrix, int maxNum){
+void fillMatrix(vector<vector<int>>& matrix, int maxNum) {
+  srand(time(0));
 
-    srand(time(0));
-
-    for(int i = 0; i < matrix.size(); i++){
-       for(int j = 0; j < matrix[i].size(); j++){
-            matrix[i][j] = (rand() % maxNum) + 1;
-        } 
+  for (int i = 0; i < matrix.size(); i++) {
+    for (int j = 0; j < matrix[i].size(); j++) {
+      matrix[i][j] = (rand() % maxNum) + 1;
     }
+  }
 }
 
 // show the matrix that was passed on the terminal's scream
-void showMatrix(vector<vector<int>>& matrix){
-
-    for(int i = 0; i < matrix.size(); i++){
-        for(int j = 0; j < matrix[i].size(); j++){
-            cout << matrix[i][j] << "\t";
-        } 
-
-        cout << "\n";
+void showMatrix(vector<vector<int>>& matrix) {
+  for (int i = 0; i < matrix.size(); i++) {
+    for (int j = 0; j < matrix[i].size(); j++) {
+      cout << matrix[i][j] << "\t";
     }
+
+    cout << "\n";
+  }
 }
 
-bool checkMatrix(vector<vector<int>>& matrix){
-	int min = 0;
-	int max = 0;
-	int sumColumns = 0;
-	int productLines = 1;
+bool checkMatrix(vector<vector<int>>& matrix) {
+  int min = 0;
+  int max = 0;
+  int sumColumns = 0;
+  int productLines = 1;
 
-	cout << "Sums of the collumn values: ";
-	for(int j = 0; j < matrix[0].size(); j++){
-		for(int i = 0; i < matrix.size(); i++){
+  cout << "Sums of the column values: ";
+  for (int j = 0; j < matrix[0].size(); j++) {
+    for (int i = 0; i < matrix.size(); i++) {
+      // increments the sum of the columns with absolute values
+      sumColumns += fabs(matrix[i][j]);
+    }
 
-			// increments the sum of the collumns with absolute values
-			sumColumns += fabs(matrix[i][j]);
-		}
+    cout << sumColumns << " - ";
 
-		cout << sumColumns << " - ";
+    // check if sum was minor than 'min' or 'min' wasn't incremented yet
+    if (sumColumns < min || min == 0) {
+      min = sumColumns;
+    }
 
-		// check if sum was minor than 'min' or 'min' wasn't incremented yet
-		if(sumColumns < min || min == 0){
-			min = sumColumns;
-		}
+    // erase number from the variable for the next loop
+    sumColumns = 0;
+  }
 
-		// erase number from the variable for the next loop
-		sumColumns = 0;
-	}
+  cout << "\nProducts of the row values:";
+  for (int i = 0; i < matrix.size(); i++) {
+    for (int j = 0; j < matrix[0].size(); j++) {
+      // increments the produtct with values from the rows
+      productLines *= matrix[i][j];
+    }
 
-	cout << "\nProducts of the row values:";
-	for(int i = 0; i < matrix.size(); i++){
-		for(int j = 0; j < matrix[0].size(); j++){
+    cout << productLines << " - ";
 
-			// increments the produtct with values from the rows
-			productLines *= matrix[i][j];
-		}
+    // check if product was greater than 'max'
+    if (productLines > max) {
+      max = productLines;
+    }
 
-		cout << productLines << " - ";
+    // erase number from the variable for the next loop
+    productLines = 1;
+  }
 
-		// check if product was greater than 'max'
-		if(productLines > max){
-			max = productLines;
-		}
+  // print values for easy checking
+  cout << "\n";
+  cout << "Min sum: " << min << "\n";
+  cout << "Max product: " << max << "\n";
 
-		// erase number from the variable for the next loop
-		productLines = 1;
-	}
-
-	// print values for easy checking
-	cout << "\n";
-	cout << "Min sum: " << min  << "\n";
-	cout << "Max product: "<< max  << "\n";
-
-	if(min <= max){
-		return true;
-	}
-	else{
-		return false;
-	}
+  if (min <= max) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
 
 int main() {
-	int n = 4;
-	int m = 5;
-    int maxNum = 21;
-    vector<vector<int>> matrix(m, vector<int>(n));
+  int n = 4;
+  int m = 5;
+  int maxNum = 21;
+  vector<vector<int>> matrix(m, vector<int>(n));
 
-	fillMatrix(matrix, maxNum);
+  fillMatrix(matrix, maxNum);
 
-	showMatrix(matrix);
+  showMatrix(matrix);
 
-	if(checkMatrix(matrix)){
-		cout << "Condicao Satisfeita";
-	}
-	else{
-		cout << "Condicao Nao Satisfeita";
-	}
+  if (checkMatrix(matrix)) {
+    cout << "Condicao Satisfeita";
+  } else {
+    cout << "Condicao Nao Satisfeita";
+  }
 
-	return 0;
+  return 0;
 }
