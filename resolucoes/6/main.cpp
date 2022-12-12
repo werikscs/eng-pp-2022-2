bc7ea642d11895917f66083c41f555ac3aede0c9
davicarvsilva <davicarv13@gmail.com>
2022-12-12 08:41:36 -0300

    feat: implements exercise 6

--- resolucoes/6/main.cpp
+++ resolucoes/6/main.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+// print vector on the screen
 void showVector(vector<int>&vector){
     for(int i = 0; i < vector.size(); i++){
         cout << vector[i] << " ";
@@ -14,6 +15,7 @@ void showVector(vector<int>&vector){
 int checkCompositeNumber(int number){
     int dividers = 0;
 
+    // for loop counts how many natural dividers the number has
     for(int i = 1; i <= number; i++){
         if(number % i == 0){
             dividers++;
@@ -28,24 +30,39 @@ int checkCompositeNumber(int number){
     }
 }
 
+
+// puts composite numbers on the begginning of the vector
 int organizeVector(vector<int>& vector, int i){
-    if(i < vector.size()){
+
+    // checks if 'i' reached the end of the vector
+    if(i < (vector.size()-1)){
+
+        // if vector[i] is not a composite number, will call
+        // the function recursively until it finds a composite number,
+        // which position will be returned at the end of the function.
+        // then swaps the positions with the help of an auxiliar variable
         if(!checkCompositeNumber(vector[i])){
             int j = organizeVector(vector, i+1);
             int aux = vector[i];
             vector[i] = vector[j];
             vector[j] = aux;
 
-            showVector(vector);
-                
-            cout << "\n";
+            // call the function again to do the same for the rest of the numbers,
+            // until it reaches the end of the vector
+            organizeVector(vector, i+1);
             
+        }
+        else{
+            // if number is a composite number, call the function for the next number,
+            // but don't swap the numbers, obviously
+            organizeVector(vector, i+1);
         }
     }
 
     return i;
 }
 
+// ask user to input numbers
 void fillVector(vector<int>& vector){
     int value;
 
@@ -55,6 +72,7 @@ void fillVector(vector<int>& vector){
 
         cin >> value;
 
+        // check if input was valid
         while(value <= 1){
             cout << "Invalid number!\n";
             i--;
@@ -65,10 +83,10 @@ void fillVector(vector<int>& vector){
 }
 
 int main() {
-    int vectorSize = 3;
-    vector<int> vector = {5, 3, 7, 8, 12, 7, 21, 9, 31, 43, 56};
+    int vectorSize = 20;
+    vector<int> vector(vectorSize); //= {5, 3, 7, 8, 12, 7, 21, 9, 31, 43, 56};
 
-    //fillVector(vector);
+    fillVector(vector);
 
     cout << "Organizando vetor\n";
 
