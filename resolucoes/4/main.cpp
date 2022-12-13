67c6927f9f9224ef65709d6bc6c536920141d8d3
davicarvsilva <davicarv13@gmail.com>
2022-12-13 13:03:38 -0300

    refactor: adds comments and changes the logic of the sortVectorNonAscending function

--- resolucoes/4/main.cpp
+++ resolucoes/4/main.cpp
@@ -3,56 +3,64 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
 
 using namespace std;
 
 // fill vector with random numbers
-void fillVectorWithRandomNumbers(int * vector, int vectorSize);
+void fillVectorWithRandomNumbers(vector<int>& vector, int maxNum);
 // sort vector by non-ascending order
-void sortVectorNonAscending(int * vector, int vectorSize);
+void sortVectorNonAscending(vector<int>& vector);
 // print vector before and after sorting
-void printVector(int * vector, int vectorSize);
+void printVector(vector<int>& vector);
 
 int main() {
   int vectorSize = 30;
-	int vector[vectorSize] = {};
+  int maxNum = 30;
+	vector<int> vector(vectorSize);
   
   // filling
-  fillVectorWithRandomNumbers(vector, vectorSize);
+  fillVectorWithRandomNumbers(vector, maxNum);
   // before sorting
-  printVector(vector, vectorSize);
+  printVector(vector);
   // sorting
-  sortVectorNonAscending(vector, vectorSize);
+  sortVectorNonAscending(vector);
   // after sorting
-  printVector(vector, vectorSize);
+  printVector(vector);
 }
 
-void fillVectorWithRandomNumbers(int * vector, int vectorSize) {
+void fillVectorWithRandomNumbers(vector<int>& vector, int maxNum) {
   srand(time(NULL));
 
-  for(int i=1; i<= vectorSize; i++)
-    vector[i-1] = 1 + (rand() % vectorSize);
+  for(int i = 0; i < vector.size(); i++)
+    vector[i] = (rand() % maxNum) + 1;
 }
 
-void sortVectorNonAscending(int * vector, int vectorSize) {
-  int i, key, j;
+void sortVectorNonAscending(vector<int>& vector) {
+  int i, j, maxPosition, aux;
 
-  for(i = 1; i < vectorSize; i++) {
-    key = vector[i];
-    j = i - 1;
+  for(i = 0; i < vector.size(); i++) {
 
-    while(j >= 0 && vector[j] > key) {
-      vector[j + 1] = vector[j];
-      j = j - 1;
+    maxPosition = i;
+
+    // loops through the rest of the vector and gets the position of the major value
+    for(int j = i; j < vector.size(); j++){
+        if(vector[j] >= vector[maxPosition]){
+            maxPosition = j;
+        }
     }
 
-    vector[j + 1] = key;
+    // swaps values
+    aux = vector[i];
+    vector[i] = vector[maxPosition];
+    vector[maxPosition] = aux;
   }
 }
 
-void printVector(int * vector, int vectorSize) {
-  for(int i = 0; i < vectorSize; i++)
+void printVector(vector<int>& vector) {
+  for(int i = 0; i < vector.size(); i++){
     cout << vector[i] << " ";
-  
+  }
+
   cout << endl;
 }
