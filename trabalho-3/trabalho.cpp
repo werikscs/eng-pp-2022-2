53040766452ee334dae9563dae58c9ca1ed1ab2d
davicarvsilva <davicarv13@gmail.com>
2023-03-22 10:33:32 -0300

    refactor: changes comments

--- trabalho-3/trabalho.cpp
+++ trabalho-3/trabalho.cpp
@@ -7,10 +7,9 @@
 using namespace std;
 
 // declare global variables
-int counter = 0;
 sem_t semSum, semSub, semMult, semDiv;
 
-// function to perform operation 2
+// performs sum
 void sum(int a, int b, int time) {
     sem_wait(&semSum);
     this_thread::sleep_for(chrono::seconds(time));
@@ -18,7 +17,7 @@ void sum(int a, int b, int time) {
     sem_post(&semSub);
 }
 
-// function to perform operation 2
+// performs subtraction
 void sub(int a, int b, int time) {
     sem_wait(&semSub);
     this_thread::sleep_for(chrono::seconds(time));
@@ -26,7 +25,7 @@ void sub(int a, int b, int time) {
     sem_post(&semMult);
 }
 
-// function to perform operation 2
+// performs multiplication
 void mult(int a, int b, int time) {
     sem_wait(&semMult);
     this_thread::sleep_for(chrono::seconds(time));
@@ -34,7 +33,7 @@ void mult(int a, int b, int time) {
     sem_post(&semDiv);
 }
 
-// function to perform operation 2
+// performs division
 void division(int a, int b, int time) {
     sem_wait(&semDiv);
     this_thread::sleep_for(chrono::seconds(time));
@@ -43,7 +42,7 @@ void division(int a, int b, int time) {
 }
 
 int main() {
-    // initialize semaphores with initial values
+    // initializes the semaphores
     sem_init(&semSum, 0, 1);
     sem_init(&semSub, 0, 0);
     sem_init(&semMult, 0, 0);
@@ -83,14 +82,14 @@ int main() {
     cout << "Mult Time: " << mult_time << endl;
     cout << "Division Time: " << division_time << endl;
 
-    for(int i = 0; i < N; i++){
-        // create two threads
+    for(int i = 1; i <= N; i++){
+        cout << "Iteration number: " << i <<endl;
+
         thread t1(sum, a, b, sum_time);
         thread t2(sub, a, b, sub_time);
         thread t3(mult, a, b, mult_time);
         thread t4(division, a, b, division_time);
 
-        // wait for threads to finish
         t1.join();
         t2.join();
         t3.join();
