e835953e1a0e5be2f6d5cfd64c43032b42e45866
Israel David <israel-david@outlook.com.br>
2023-03-22 16:59:06 -0300

    chore: add teste

--- trabalho-3/trabalho.cpp
+++ trabalho-3/trabalho.cpp
@@ -1,22 +1,57 @@
 #include <chrono>
 #include <iostream>
-#include <semaphore.h>
+#include <semaphore>
 #include <thread>
 
 using namespace std;
 
-void soma(float, float, int, int, sem_t *, sem_t *);
-void subt(float, float, int, int, sem_t *, sem_t *);
-void mult(float, float, int, int, sem_t *, sem_t *);
-void divi(float, float, int, int, sem_t *, sem_t *);
+void soma(float a, float b, int time, int N, std::counting_semaphore<1> &sem_to_lock, std::counting_semaphore<1> &sem_to_unlock)
+{
+  for (int i = 0; i < N; i++)
+  {
+    sem_to_lock.acquire();
+    this_thread::sleep_for(chrono::seconds(time));
+    cout << "N: " << i + 1 << ", Op - So: " << a << " + " << b << " = " << a + b << endl;
+    sem_to_unlock.release();
+  }
+}
 
-int main() {
-  sem_t semSo, semSu, semMu, semDi;
-  
-  sem_init(&semSo, 0, 1);
-  sem_init(&semSu, 0, 0);
-  sem_init(&semMu, 0, 0);
-  sem_init(&semDi, 0, 0);
+void subt(float a, float b, int time, int N, std::counting_semaphore<1> &sem_to_lock, std::counting_semaphore<1> &sem_to_unlock)
+{
+  for (int i = 0; i < N; i++)
+  {
+    sem_to_lock.acquire();
+    this_thread::sleep_for(chrono::seconds(time));
+    cout << "N: " << i + 1 << ", Op - Su: " << a << " - " << b << " = " << a - b << endl;
+    sem_to_unlock.release();
+  }
+}
+
+void mult(float a, float b, int time, int N, std::counting_semaphore<1> &sem_to_lock, std::counting_semaphore<1> &sem_to_unlock)
+{
+  for (int i = 0; i < N; i++)
+  {
+    sem_to_lock.acquire();
+    this_thread::sleep_for(chrono::seconds(time));
+    cout << "N: " << i + 1 << ", Op - Mu: " << a << " x " << b << " = " << a * b << endl;
+    sem_to_unlock.release();
+  }
+}
+
+void divi(float a, float b, int time, int N, std::counting_semaphore<1> &sem_to_lock, std::counting_semaphore<1> &sem_to_unlock)
+{
+  for (int i = 0; i < N; i++)
+  {
+    sem_to_lock.acquire();
+    this_thread::sleep_for(chrono::seconds(time));
+    cout << "N: " << i + 1 << ", Op - Di: " << a << " / " << b << " = " << a / b << endl;
+    sem_to_unlock.release();
+  }
+}
+
+int main()
+{
+  counting_semaphore<1> semSo(1), semSu(1), semMu(1), semDi(1);
 
   float a, b = 0;
   int N = 0;
@@ -24,12 +59,14 @@ int main() {
   cout << "Digite um numero 'a': ";
   cin >> a;
 
-  while (b == 0) {
+  while (b == 0)
+  {
     cout << "Digite um número 'b' ('b' ≠ 0): ";
     cin >> b;
   }
 
-  while (N < 1) {
+  while (N < 1)
+  {
     cout << "Digite um número 'N' (N >= 1): ";
     cin >> N;
   }
@@ -50,56 +87,15 @@ int main() {
 
   cout << "---------------------------" << endl;
 
-  thread t1(soma, a, b, so_time, N, &semSo, &semSu);
-  thread t2(subt, a, b, su_time, N, &semSu, &semMu);
-  thread t3(mult, a, b, mu_time, N, &semMu, &semDi);
-  thread t4(divi, a, b, di_time, N, &semDi, &semSo);
+  thread t1(soma, a, b, so_time, N, std::ref(semSo), std::ref(semSu));
+  thread t2(subt, a, b, su_time, N, std::ref(semSu), std::ref(semMu));
+  thread t3(mult, a, b, mu_time, N, std::ref(semMu), std::ref(semDi));
+  thread t4(divi, a, b, di_time, N, std::ref(semDi), std::ref(semSo));
 
   t1.join();
   t2.join();
   t3.join();
   t4.join();
 
-  sem_destroy(&semSo);
-  sem_destroy(&semSu);
-  sem_destroy(&semMu);
-  sem_destroy(&semDi);
-
   return 0;
-}
-
-void soma(float a, float b, int time, int N, sem_t *sem_to_lock, sem_t *sem_to_unlock) {
-  for (int i = 0; i < N; i++) {
-    sem_wait(sem_to_lock);
-    this_thread::sleep_for(chrono::seconds(time));
-    cout << "N: " << i+1 << ", Op - So: " << a << " + " << b << " = " << a+b << endl;
-    sem_post(sem_to_unlock);
-  }
-}
-
-void subt(float a, float b, int time, int N, sem_t *sem_to_lock, sem_t *sem_to_unlock) {
-  for (int i = 0; i < N; i++) {
-    sem_wait(sem_to_lock);
-    this_thread::sleep_for(chrono::seconds(time));
-    cout << "N: " << i+1 << ", Op - Su: " << a << " - " << b << " = " << a-b << endl;
-    sem_post(sem_to_unlock);
-  }
-}
-
-void mult(float a, float b, int time, int N, sem_t *sem_to_lock, sem_t *sem_to_unlock) {
-  for (int i = 0; i < N; i++) {
-    sem_wait(sem_to_lock);
-    this_thread::sleep_for(chrono::seconds(time));
-    cout << "N: " << i+1 << ", Op - Mu: " << a << " x " << b << " = " << a*b << endl;
-    sem_post(sem_to_unlock);
-  }
-}
-
-void divi(float a, float b, int time, int N, sem_t *sem_to_lock, sem_t *sem_to_unlock) {
-  for (int i = 0; i < N; i++) {
-    sem_wait(sem_to_lock);
-    this_thread::sleep_for(chrono::seconds(time));
-    cout << "N: " << i+1 << ", Op - Di: " << a << " / " << b << " = " << a/b << endl;
-    sem_post(sem_to_unlock);
-  }
 }
