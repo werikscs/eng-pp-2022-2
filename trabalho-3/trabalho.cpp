c1bfa449b1ffe8ce18069b66c4a1389e2c978303
Davi Carvalho <38366263+davicarvsilva@users.noreply.github.com>
2023-03-22 18:12:47 -0300

    update: changes semaphores lib for mutex and condition vars

--- trabalho-3/trabalho.cpp
+++ trabalho-3/trabalho.cpp
@@ -1,98 +1,107 @@
-#include <chrono>
 #include <iostream>
-#include <semaphore>
+#include <chrono>
 #include <thread>
+#include <mutex>
+#include <condition_variable>
 
 using namespace std;
 
-void soma(binary_semaphore *, binary_semaphore *, float, float, int, int);
-void subt(binary_semaphore *, binary_semaphore *, float, float, int, int);
-void mult(binary_semaphore *, binary_semaphore *, float, float, int, int);
-void divi(binary_semaphore *, binary_semaphore *, float, float, int, int);
+void sum(float, float, int, int, int, int*, mutex&, condition_variable&);
+void subt(float, float, int, int, int, int*, mutex&, condition_variable&);
+void mult(float, float, int, int, int, int*, mutex&, condition_variable&);
+void divi(float, float, int, int, int, int*, mutex&, condition_variable&);
 
 int main() {
-  binary_semaphore sem_So{0};
-  binary_semaphore sem_Su{0};
-  binary_semaphore sem_Mu{0};
-  binary_semaphore sem_Di{0};
 
-  float a, b = 0;
-  int N = 0;
+    float a, b = 0;
+    int N = 0;
 
-  cout << "Digite um numero 'a': ";
-  cin >> a;
+    mutex mtx;
+    condition_variable cv;
+    int turn = 1;
 
-  while (b == 0) {
-    cout << "Digite um número 'b' ('b' ≠ 0): ";
-    cin >> b;
-  }
+    cout << "Digite um numero 'a': ";
+    cin >> a;
 
-  while (N < 1) {
-    cout << "Digite um número 'N' (N >= 1): ";
-    cin >> N;
-  }
+    while (b == 0) {
+        cout << "Digite um número 'b' ('b' ≠ 0): ";
+        cin >> b;
+    }
 
-  cout << "---------------------------" << endl;
+    while (N < 1) {
+        cout << "Digite um número 'N' (N >= 1): ";
+        cin >> N;
+    }
 
-  srand(1032013);
+    cout << "---------------------------" << endl;
 
-  int so_time = rand() % 10 + 1;
-  int su_time = rand() % 10 + 1;
-  int mu_time = rand() % 10 + 1;
-  int di_time = rand() % 10 + 1;
+    srand(1032013);
 
-  cout << "Tempo - So: " << so_time << "s" << endl;
-  cout << "Tempo - Su: " << su_time << "s" << endl;
-  cout << "Tempo - Mu: " << mu_time << "s" << endl;
-  cout << "Tempo - Di: " << di_time << "s" << endl;
+    int so_time = rand() % 10 + 1;
+    int su_time = rand() % 10 + 1;
+    int mu_time = rand() % 10 + 1;
+    int di_time = rand() % 10 + 1;
 
-  cout << "---------------------------" << endl;
+    cout << "Tempo - So: " << so_time << "s" << endl;
+    cout << "Tempo - Su: " << su_time << "s" << endl;
+    cout << "Tempo - Mu: " << mu_time << "s" << endl;
+    cout << "Tempo - Di: " << di_time << "s" << endl;
 
-  thread th_so(soma, &sem_So, &sem_Su, a, b, so_time, N);
-  thread th_su(subt, &sem_Su, &sem_Mu, a, b, su_time, N);
-  thread th_mu(mult, &sem_Mu, &sem_Di, a, b, mu_time, N);
-  thread th_di(divi, &sem_Di, &sem_So, a, b, di_time, N);
+    cout << "---------------------------" << endl;
 
-  sem_So.release();
+    thread t1(sum, a, b, so_time, N, 1, &turn, ref(mtx), ref(cv));
+    thread t2(subt, a, b, su_time, N, 2, &turn, ref(mtx), ref(cv));
+    thread t3(mult, a, b, mu_time, N, 3, &turn, ref(mtx), ref(cv));
+    thread t4(divi, a, b, di_time, N, 4, &turn, ref(mtx), ref(cv));
 
-  th_so.join();
-  th_su.join();
-  th_mu.join();
-  th_di.join();
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+
+    return 0;
 }
 
-void soma(binary_semaphore * sem_to_acquire, binary_semaphore * sem_to_release, float a, float b, int time, int N) {
-  for (int i = 0; i < N; i++) {
-    sem_to_acquire->acquire();
-    this_thread::sleep_for(chrono::seconds(time));
-    cout << "N: " << i + 1 << ", Op - So: " << a << " + " << b << " = " << a + b << endl;     
-    sem_to_release->release(); 
-  }
+void sum(float a, float b, int time, int N, int id, int* turn, mutex& mtx, condition_variable& cv) {
+    for (int i = 0; i < N; i++) {
+        unique_lock<mutex> lock(mtx);
+        cv.wait(lock, [&turn, id]() { return (*turn == id); });
+        this_thread::sleep_for(chrono::seconds(time));
+        cout << "N: " << i+1 << ", Op - So: " << a << " + " << b << " = " << a+b << endl;
+        *turn = (*turn % 4) + 1;
+        cv.notify_all();
+    }
 }
 
-void subt(binary_semaphore * sem_to_acquire, binary_semaphore * sem_to_release, float a, float b, int time, int N) {
-  for (int i = 0; i < N; i++) {
-    sem_to_acquire->acquire();
-    this_thread::sleep_for(chrono::seconds(time));
-    cout << "N: " << i + 1 << ", Op - Su: " << a << " - " << b << " = " << a - b << endl;     
-    sem_to_release->release(); 
-  }
+void subt(float a, float b, int time, int N, int id, int* turn, mutex& mtx, condition_variable& cv) {
+    for (int i = 0; i < N; i++) {
+        unique_lock<mutex> lock(mtx);
+        cv.wait(lock, [&turn, id]() { return (*turn == id); });
+        this_thread::sleep_for(chrono::seconds(time));
+        cout << "N: " << i+1 << ", Op - Su: " << a << " - " << b << " = " << a-b << endl;
+        *turn = (*turn % 4) + 1;
+        cv.notify_all();
+    }
 }
 
-void mult(binary_semaphore * sem_to_acquire, binary_semaphore * sem_to_release, float a, float b, int time, int N) {
-  for (int i = 0; i < N; i++) {
-    sem_to_acquire->acquire();
-    this_thread::sleep_for(chrono::seconds(time));
-    cout << "N: " << i + 1 << ", Op - Mu: " << a << " x " << b << " = " << a * b << endl;
-    sem_to_release->release(); 
-  }
+void mult(float a, float b, int time, int N, int id, int* turn, mutex& mtx, condition_variable& cv) {
+    for (int i = 0; i < N; i++) {
+        unique_lock<mutex> lock(mtx);
+        cv.wait(lock, [&turn, id]() { return (*turn == id); });
+        this_thread::sleep_for(chrono::seconds(time));
+        cout << "N: " << i+1 << ", Op - Mu: " << a << " x " << b << " = " << a*b << endl;
+        *turn = (*turn % 4) + 1;
+        cv.notify_all();
+    }
 }
 
-void divi(binary_semaphore * sem_to_acquire, binary_semaphore * sem_to_release, float a, float b, int time, int N) {
-  for (int i = 0; i < N; i++) {
-    sem_to_acquire->acquire();
-    this_thread::sleep_for(chrono::seconds(time));
-    cout << "N: " << i + 1 << ", Op - Di: " << a << " / " << b << " = " << a / b << endl;
-    sem_to_release->release(); 
-  }
+void divi(float a, float b, int time, int N, int id, int* turn, mutex& mtx, condition_variable& cv) {
+    for (int i = 0; i < N; i++) {
+        unique_lock<mutex> lock(mtx);
+        cv.wait(lock, [&turn, id]() { return (*turn == id); });
+        this_thread::sleep_for(chrono::seconds(time));
+        cout << "N: " << i+1 << ", Op - Di: " << a << " / " << b << " = " << a/b << endl;
+        *turn = (*turn % 4) + 1;
+        cv.notify_all();
+    }
 }
